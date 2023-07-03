c54e29221d639e421b57a7840416a60a200714ec
sree-lekha-7 <138370659+sree-lekha-7@users.noreply.github.com>
2023-07-04 03:37:42 +0530

    Update code_df.v

--- code_df.v
+++ code_df.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/02/2023 06:57:29 PM
-// Design Name: 
-// Module Name: code_df
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module code_df(Z,Z_sa0,net_f_sa0,A,B,C,D);
 input net_f_sa0,A,B,C,D;
 output Z,Z_sa0;
